8a1a45ee0815e4977bfd15416a94b1a31ef48ec9
yasht <yashtandon007@gmail.com>
2024-07-16 22:49:53 +0530

    //tom2

--- app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesState.kt
+++ app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesState.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication.feature_note.presentation.add_edit_note
 
-
+//tom2
 data class AddEditNotesState(
     val title: NoteTextFieldState,
     val content: NoteTextFieldState,
