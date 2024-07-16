bd88d215b94629f18f194f067a96f73531d78218
yasht <yashtandon007@gmail.com>
2024-07-16 16:05:44 +0530

    //lcck

--- app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesEvent.kt
+++ app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesEvent.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.feature_note.presentation.add_edit_note
 import androidx.compose.ui.focus.FocusState
 
 
-sealed class AddEditNotesEvent{
+sealed class //lcckAddEditNotesEvent{
     data object SaveNote:AddEditNotesEvent()
     data class EnterTitle(val title: String):AddEditNotesEvent()
     data class EnterContent(val content: String):AddEditNotesEvent()
