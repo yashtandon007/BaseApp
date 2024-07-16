c0c4dfa094458951c91761804dea509b86abc52b
yasht <yashtandon007@gmail.com>
2024-07-16 14:51:25 +0530

    beta1

--- app/src/main/java/com/example/myapplication/NoteApp.kt
+++ app/src/main/java/com/example/myapplication/NoteApp.kt
@@ -2,7 +2,7 @@ package com.example.myapplication
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-
+//papa2
 @HiltAndroidApp
 class NoteApp: Application() //child
 //papa
