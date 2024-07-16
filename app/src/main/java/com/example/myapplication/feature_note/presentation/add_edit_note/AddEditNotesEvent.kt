27f6de367c996ee43404d3146d458c8da1f12245
yasht <yashtandon007@gmail.com>
2024-07-16 21:25:21 +0530

    //nail3

--- app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesEvent.kt
+++ app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesEvent.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.feature_note.presentation.add_edit_note
 import androidx.compose.ui.focus.FocusState
 
 //nail1
+//nail3
 sealed class AddEditNotesEvent{
     data object SaveNote:AddEditNotesEvent()
     data class EnterTitle(val title: String):AddEditNotesEvent()
