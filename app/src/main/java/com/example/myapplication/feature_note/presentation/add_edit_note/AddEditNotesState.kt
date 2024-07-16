d0298b62e27d854c9cc4552feb8074e904ae84c1
yasht <yashtandon007@gmail.com>
2024-07-16 20:29:31 +0530

    //shoe3

--- app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesState.kt
+++ app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesState.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.feature_note.presentation.add_edit_note
 
 //shoe
+//shoe3
 data class AddEditNotesState(
     val title: NoteTextFieldState,
     val content: NoteTextFieldState,
