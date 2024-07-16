75867e5ccb362e8b784009417babe6a0eac09614
yasht <yashtandon007@gmail.com>
2024-07-16 15:09:20 +0530

    2

--- app/src/main/java/com/example/myapplication/NoteApp.kt
+++ app/src/main/java/com/example/myapplication/NoteApp.kt
@@ -2,8 +2,6 @@ package com.example.myapplication
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-
+//2
 @HiltAndroidApp
 class NoteApp: Application()
-//master
-//master
