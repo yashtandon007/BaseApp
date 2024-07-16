563fd72de0701ca46c2797d9c6e528a236c414f4
yasht <yashtandon007@gmail.com>
2024-07-17 00:32:30 +0530

    fNext

--- app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesState.kt
+++ app/src/main/java/com/example/myapplication/feature_note/presentation/add_edit_note/AddEditNotesState.kt
@@ -4,7 +4,7 @@ package com.example.myapplication.feature_note.presentation.add_edit_note
 data class AddEditNotesState(
     val title: NoteTextFieldState,
     val content: NoteTextFieldState,
-    val color: Int,
+    val color: Int,//f mext
     val currentNoteId: Int? = null
 )
 //dev2
